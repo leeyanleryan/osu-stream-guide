6571c81602e6d497707ec65285c16a102e71ef10
Ryan Lee <leeyanleryan.21@gmail.com>
2026-08-02 21:41:27 +0800

    Add dark mode to local storage

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,10 +1,21 @@
+function applyStoredTheme() {
+  const theme = localStorage.getItem("theme");
+  if (theme === "dark") {
+    document.body.classList.add("dark-mode");
+  }
+}
+
+applyStoredTheme();
+
 const pullChain = document.getElementById("pull-chain");
 
 pullChain.addEventListener("click", () => {
   if (document.body.classList.contains("dark-mode")) {
     document.body.classList.remove("dark-mode");
+    localStorage.setItem("theme", "light");
   }
   else {
     document.body.classList.add("dark-mode");
+    localStorage.setItem("theme", "dark");
   }
 });
